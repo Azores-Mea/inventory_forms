2505359ae3d607e9ff15927388563a8624f556a9
Azores-Mea <151590493+Azores-Mea@users.noreply.github.com>
2024-12-05 00:14:06 +0800

    Add files via upload

---
+++ INVENTORYSYSTEM_GROUP2/INVENTORYSYSTEM_GROUP2/Reload.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INVENTORYSYSTEM_GROUP2
+{
+    public interface Reload
+    {
+        void LoadData();
+    }
+}
